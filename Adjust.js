c969637c519c442309018ae36f27908d878d53b8
Brutenis Jusaitis <b.jusaitis@gmail.com>
2020-09-27 00:41:41 +0100

    Extended 'equalize' method, added width and height possibilities

--- Adjust.js
+++ Adjust.js
@@ -1,6 +1,7 @@
 class Adjust {
+
   constructor() {
-    this.version = "1.0";
+    this.version = "1.0.0";
     this.name = `${this.constructor.name}.js`;
     console.info(`This site is running version ${this.version} of ${this.name}!`);
   }
@@ -10,7 +11,7 @@ class Adjust {
 
     switch (error) {
       case "classNotFound":
-        message = `${this.name}: The DOM doesn't seem to have elements with a class name of "${name}`;
+        message = `${this.name}: The DOM doesn't seem to have elements with a class name of "${name}"`;
         break;
       
       case "noClassSet":
@@ -23,50 +24,86 @@ class Adjust {
 
       case "notNumber":
         message = `${this.name}: The "${name}" parameter is not a number!`;
+        break;
+      
+      case "typeNotFound":
+        message = `${this.name}: The "type" parameter has to be ${name}!`;
     }
 
     console.warn(message);
   }
 
-  equalizeHeights(data) {
+  equalizer(data) {
     if (data) {
       if (data.className) {
         if (document.getElementsByClassName(data.className)) {
-          const elements = Array.from(document.getElementsByClassName(data.className));
-          let height = 0;
+          if (data.type && (data.type.toLowerCase() == "height" || data.type.toLowerCase() == "width")) {
+            const elements = Array.from(document.getElementsByClassName(data.className));
+            let max = 0;
+  
+            const minWidth = data.minWidth ? data.minWidth : 0;
+            const maxWidth = data.maxWidth ? data.maxWidth : Number.MAX_VALUE;
+  
+            const execute = () => {
+              max = 0;
+              
+              if (window.innerWidth >= minWidth && window.innerWidth <= maxWidth) {
 
-          const minWidth = data.minWidth ? data.minWidth : 0;
-          const maxWidth = data.maxWidth ? data.maxwidth : Number.MAX_VALUE;
+                if (data.type == "height") {
+                  elements.forEach(element => {
+                    element.style.height = "auto";
+                    if (element.clientHeight > max) {
+                      max = element.clientHeight;
+                    }
+                  })
 
-          const execute = () => {
-            if (window.innerWidth > minWidth && window.innerWidth < maxWidth) {
-              elements.forEach(element => {
-                element.style.height = "auto";
-
-                if (element.clientHeight > height) {
-                  height = element.clientHeight;
+                  elements.forEach(element => {
+                    element.style.height = `${max}px`;
+                  })
                 }
-              })
+                
+                if (data.type == "width") {
+                  elements.forEach(element => {
+                    element.style.width = "auto";
+                    if (element.clientWidth > max) {
+                      max = element.clientWidth;
+                    }
+                  })
 
-              elements.forEach(element => {
-                element.style.height = height;
-              })
-            } else {
-              elements.forEach(element => {
-                element.style.height = "auto";
-              })
+                  elements.forEach(element => {
+                    element.style.height = `${max}px`;
+                  })
+                }
+
+              } else {
+
+                if (data.type == "height") {
+                  elements.forEach(element => {
+                    element.style.height = "auto";
+                  })
+                }
+
+                if (data.type == "width") {
+                  elements.forEach(element => {
+                    element.style.width = "auto";
+                  })
+                }
+
+              }
             }
-          }
+  
+            if (typeof minWidth != "number") {
+              this.warn("notNumber", "minWidth");
+            } else if (typeof maxWidth != "number") {
+              this.warn("notNumber", "maxWidth");
+            } else {
+              window.addEventListener("load", execute);
+              window.addEventListener("resize", execute);
+            }
 
-          if (typeof minWidth != "number") {
-            this.warn("notNumber", "minWidth");
-          } else if (typeof maxWidth != "number") {
-            this.warn("notNumber", "maxWidth");
           } else {
-            window.addEventListener("load", execute);
-            window.addEventListener("resize", execute);
+            this.warn("typeNotFound", `"width" or "height"`);
           }
-
         } else {
           this.warn("classNotFound", data.className);
         }
